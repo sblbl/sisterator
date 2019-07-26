d9da3b29bd1e4b35351cf1b3501ac65e33979c6b
mariavittoriacampodonico26@gmail.com <marta.fioravanti.1996@gmail.com>
2019-07-27 01:44:50 +0200

    added style, new parametric functions

--- script.js
+++ script.js
@@ -1,3 +1,32 @@
+let $_nun = $('#nun'),
+	$_body = $('#body'),
+	$_shoulders = $('#shoulders'),
+	$_trunk = $('#trunk'),
+	$_visage = $('#visage'),
+	$_face = $('#face'),
+	$_right_eye = $('#right-eye'),
+	$_left_eye = $('#left-eye'),
+	$_veil = $('#back-veil')
+
+let $_face_roundness = $('input#face-roundness'),
+	$_nun_height = $('input#nun-height'),
+	$_nun_width = $('input#nun-width'),
+	$_eye_height = $('input#eye-height'),
+	$_eye_width = $('input#eye-width')
+
+
 jQuery(document).ready(function($){
-	
+	$_face_roundness.on('mousedown', function (e) {
+		$(document).on('mousemove', change_roundness)
+		$(document).on('mouseup', end_change_roundness)
+	})
+	let change_roundness = function (e) {
+		let roundness = $_face_roundness.val() * 50
+		$_face.attr('rx', roundness)
+		$_face.attr('rx', roundness)
+	}
+	let end_change_roundness = function (e) {
+		$(document).off('mousemove', change_roundness)
+		$(document).off('mouseup', end_change_roundness)
+	}
 })
