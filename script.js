27936ef3531d9ce3d561c7e1f254b6b4f9853f2c
mariavittoriacampodonico26@gmail.com <marta.fioravanti.1996@gmail.com>
2019-07-27 19:09:16 +0200

    Added some function

--- script.js
+++ script.js
@@ -5,17 +5,24 @@ let $_nun = $('#nun'),
 	$_visage = $('#visage'),
 	$_face = $('#face'),
 	$_right_eye = $('#right-eye'),
+	$_right_eye_sclera = $('#right-eye-sclera'),
+	$_right_eye_pupil = $('#right-eye-pupil'),
 	$_left_eye = $('#left-eye'),
+	$_left_eye_sclera = $('#left-eye-sclera'),
+	$_left_eye_pupil = $('#left-eye-pupil'),
 	$_veil = $('#back-veil')
 
 let $_face_roundness = $('input#face-roundness'),
 	$_nun_height = $('input#nun-height'),
 	$_nun_width = $('input#nun-width'),
-	$_eye_height = $('input#eye-height'),
+	$_pupil_proportion = $('input#pupil-proportion'),
 	$_eye_width = $('input#eye-width')
 
 
 jQuery(document).ready(function($){
+
+	/*---------------- FACE ROUNDNESS ----------------*/
+
 	$_face_roundness.on('mousedown', function (e) {
 		$(document).on('mousemove', change_roundness)
 		$(document).on('mouseup', end_change_roundness)
@@ -29,4 +36,53 @@ jQuery(document).ready(function($){
 		$(document).off('mousemove', change_roundness)
 		$(document).off('mouseup', end_change_roundness)
 	}
+
+	/*---------------- EYE WIDTH  ----------------*/
+	$_eye_width.on('mousedown', function (e) {
+		$(document).on('mousemove', change_eye_width)
+		$(document).on('mouseup', end_change_eye_width)
+	})
+	let change_eye_width = function (e) {
+		let width = $_eye_width.val() * 5 + 30
+		$_right_eye_sclera.attr('r', width)
+		$_right_eye_pupil.attr('r', width/3*2)
+		$_left_eye_sclera.attr('r', width)
+		$_left_eye_pupil.attr('r', width/3*2)
+	}
+	let end_change_eye_width = function (e) {
+		$(document).off('mousemove', change_roundness)
+		$(document).off('mouseup', end_change_roundness)
+	}
+
+	/*---------------- PUPIL PROPORTION  ----------------*/
+
+	$_pupil_proportion.on('mousedown', function (e) {
+		$(document).on('mousemove', change_pupil_proportion)
+		$(document).on('mouseup', end_change_pupil_proportion)
+	})
+	let change_pupil_proportion = function (e) {
+		let pupil_prop = $_pupil_proportion.val()
+		let sclera_width = $_right_eye_sclera.attr('width')
+		//let width = $_eye_width.val() * 5 + 30
+	}
+	let end_change_pupil_proportion = function (e) {
+		$(document).off('mousemove', change_pupil_proportion)
+		$(document).off('mouseup', end_change_pupil_proportion)
+	}
 })
+
+/*add batman mode*/
+
+/*
+$_eye_width.on('mousedown', function (e) {
+	$(document).on('mousemove', change_eye_width)
+	$(document).on('mouseup', end_change_eye_width)
+})
+let change_eye_width = function (e) {
+	
+}
+let end_change_eye_width = function (e) {
+	$(document).off('mousemove', )
+	$(document).off('mouseup', )
+}
+*/
