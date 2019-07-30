8c8213fa2d6b39e616083f2c45e3109fc26633c4
mariavittoriacampodonico26@gmail.com <marta.fioravanti.1996@gmail.com>
2019-07-30 03:46:14 +0200

    Touch events ON!

--- script.js
+++ script.js
@@ -81,15 +81,15 @@ jQuery(document).ready(function($) {
 		$_face.attr('rx', roundness)
 		$_variable_veil.attr('height', $_face_roundness.val()*19)
 		$('#face-roundness-val').html($_face_roundness.val())
-		$(document).off('mousemove', change_roundness)
-		$(document).off('mouseup', end_change_roundness)
+		$(document).off('mousemove vmousemove', change_roundness)
+		$(document).off('mouseup vmouseup', end_change_roundness)
 	}
 
 
 	/*---------------- EYE WIDTH  ----------------*/
-	$_eye_width.on('mousedown', function (e) {
-		$(document).on('mousemove', change_eye_width)
-		$(document).on('mouseup', end_change_eye_width)
+	$_eye_width.on('mousedown vmousedown', function (e) {
+		$(document).on('mousemove vmousemove', change_eye_width)
+		$(document).on('mouseup vmouseup', end_change_eye_width)
 	})
 
 	let change_eye_width = function (e) {
@@ -103,15 +103,15 @@ jQuery(document).ready(function($) {
 	}
 
 	let end_change_eye_width = function (e) {
-		$(document).off('mousemove', change_eye_width)
-		$(document).off('mouseup', end_change_eye_width)
+		$(document).off('mousemove vmousemove', change_eye_width)
+		$(document).off('mouseup vmouseup', end_change_eye_width)
 	}
 
 	/*---------------- PUPIL PROPORTION  ----------------*/
 
-	$_pupil_proportion.on('mousedown', function (e) {
-		$(document).on('mousemove', change_pupil_proportion)
-		$(document).on('mouseup', end_change_pupil_proportion)
+	$_pupil_proportion.on('mousedown vmousedown', function (e) {
+		$(document).on('mousemove vmousemove', change_pupil_proportion)
+		$(document).on('mouseup vmouseup', end_change_pupil_proportion)
 	})
 
 	let change_pupil_proportion = function (e) {
@@ -124,15 +124,15 @@ jQuery(document).ready(function($) {
 	}
 
 	let end_change_pupil_proportion = function (e) {
-		$(document).off('mousemove', change_pupil_proportion)
-		$(document).off('mouseup', end_change_pupil_proportion)
+		$(document).off('mousemove vmousemove', change_pupil_proportion)
+		$(document).off('mouseup vmouseup', end_change_pupil_proportion)
 	}
 
 	/*---------------- MOUSTACHE LENGHT  ----------------*/
 
-	$_moustache_lenght.on('mousedown', function (e) {
-		$(document).on('mousemove', change_moustache_lenght)
-		$(document).on('mouseup', end_change_moustache_lenght)
+	$_moustache_lenght.on('mousedown vmousedown', function (e) {
+		$(document).on('mousemove vmousemove', change_moustache_lenght)
+		$(document).on('mouseup vmouseup', end_change_moustache_lenght)
 	})
 
 	let change_moustache_lenght = function (e) {
@@ -151,15 +151,15 @@ jQuery(document).ready(function($) {
 	}
 
 	let end_change_moustache_lenght = function (e) {
-		$(document).off('mousemove', change_moustache_lenght)
-		$(document).off('mouseup', end_change_moustache_lenght)
+		$(document).off('mousemove vmousemove', change_moustache_lenght)
+		$(document).off('mouseup vmouseup', end_change_moustache_lenght)
 	}
 
 	/*---------------- BODY WIDTH  ----------------*/
 
-	$_nun_width.on('mousedown', function (e) {
-		$(document).on('mousemove', change_nun_width)
-		$(document).on('mouseup', end_change_nun_width)
+	$_nun_width.on('mousedown vmousedown', function (e) {
+		$(document).on('mousemove vmousemove', change_nun_width)
+		$(document).on('mouseup vmouseup', end_change_nun_width)
 	})
 
 	let change_nun_width = function (e) {
@@ -178,15 +178,15 @@ jQuery(document).ready(function($) {
 	}
 
 	let end_change_nun_width = function (e) {
-		$(document).off('mousemove', change_nun_width)
-		$(document).off('mouseup', end_change_nun_width)
+		$(document).off('mousemove vmousemove', change_nun_width)
+		$(document).off('mouseup vmouseup', end_change_nun_width)
 	}
 
 	/*---------------- NUN DRESS  ----------------*/
 
-	$_nun_dress.on('mousedown', function (e) {
-		$(document).on('mousemove', change_nun_dress)
-		$(document).on('mouseup', end_change_nun_dress)
+	$_nun_dress.on('mousedown vmousedown', function (e) {
+		$(document).on('mousemove vmousemove', change_nun_dress)
+		$(document).on('mouseup vmouseup', end_change_nun_dress)
 	})
 
 	let change_nun_dress = function (e) {
@@ -279,8 +279,8 @@ jQuery(document).ready(function($) {
 	}
 
 	let end_change_nun_dress = function (e) {
-		$(document).off('mousemove', change_nun_dress)
-		$(document).off('mouseup', end_change_nun_dress)
+		$(document).off('mousemove vmousemove', change_nun_dress)
+		$(document).off('mouseup vmouseup', end_change_nun_dress)
 	}
 
 	$_color_button.on('click', function () {
